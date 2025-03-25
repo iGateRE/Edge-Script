2209ff70e047140474b2933dcb9ed78d399eb3be
Sathyaiplon <146912323+Sathyaiplon@users.noreply.github.com>
2025-03-25 16:51:17 +0530

    Update start-edgex.sh

--- start-edgex.sh
+++ start-edgex.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#cp /var/lib/software-update/modules/0/docker-compose.yaml ~/docker-compose.yaml
-
-#cd ~ || exit
-
 cd /var/lib/software-update/modules/0/ || exit
 
 sudo docker compose up -d
 
-#docker compose up -d
